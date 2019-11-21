cc4774fc7d9b797957272c1fb6fb2b166095c97b
Lakovych Alexey <lakovych.oleksii@pdffiller.team>
2019-11-21 16:06:59 +0200

    fixes

--- src/main/java/com/loliktest/ufit/listeners/testng/AllureAttachmentsListener.java
+++ src/main/java/com/loliktest/ufit/listeners/testng/AllureAttachmentsListener.java
@@ -4,6 +4,8 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.testng.AllureTestNg;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.ITestResult;
 
 import java.io.ByteArrayInputStream;
@@ -13,9 +15,11 @@ import static com.loliktest.ufit.UFitBrowser.browser;
 
 public class AllureAttachmentsListener extends AllureTestNg {
 
+    private Logger logger = LoggerFactory.getLogger(AllureAttachmentsListener.class);
+
     @Override
     public void onConfigurationFailure(ITestResult itr) {
-        System.out.println("UFit Allure: onConfigurationFailure");
+        logger.info("UFit Allure: onConfigurationFailure");
         final String uuid = UUID.randomUUID().toString();
         final String parentUuid = UUID.randomUUID().toString();
         startTestCase(itr, parentUuid, uuid);
@@ -26,7 +30,7 @@ public class AllureAttachmentsListener extends AllureTestNg {
 
     @Override
     public void onTestFailure(final ITestResult result) {
-        System.out.println("UFit Allure: onTestFailure");
+        logger.info("UFit Allure: onTestFailure");
         Allure.addAttachment("(UFit) Browser Screen Failed", new ByteArrayInputStream(((TakesScreenshot) browser().driver()).getScreenshotAs(OutputType.BYTES)));
         super.onTestFailure(result);
     }
