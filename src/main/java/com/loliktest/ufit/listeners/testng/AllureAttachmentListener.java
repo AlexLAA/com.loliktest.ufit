e678134ec5e471aaea70773855b82a8899393bda
Lakovych Alexey <lakovych.oleksii@pdffiller.team>
2020-01-06 12:16:43 +0200

    update version

--- src/main/java/com/loliktest/ufit/listeners/testng/AllureAttachmentListener.java
+++ src/main/java/com/loliktest/ufit/listeners/testng/AllureAttachmentListener.java
@@ -1,7 +1,9 @@
 package com.loliktest.ufit.listeners.testng;
 
 import io.qameta.allure.Allure;
+import io.qameta.allure.listener.FixtureLifecycleListener;
 import io.qameta.allure.listener.TestLifecycleListener;
+import io.qameta.allure.model.FixtureResult;
 import io.qameta.allure.model.Status;
 import io.qameta.allure.model.TestResult;
 
@@ -9,9 +11,20 @@ import java.util.stream.Collectors;
 
 import static com.loliktest.ufit.UFitBrowser.browser;
 
-public class AllureAttachmentListener implements TestLifecycleListener {
+public class AllureAttachmentListener implements TestLifecycleListener, FixtureLifecycleListener {
 
     public void beforeTestStop(TestResult result) {
+        Status status = result.getStatus();
+        if (status == Status.FAILED || status == Status.BROKEN) {
+            Allure.parameter("Browser: Failed URL", browser().getCurrentUrl());
+            browser().getScreenOnFail();
+            Allure.addAttachment("Browser: Console Logs",  browser().devTools.getConsoleErrors().stream().map(logEntry -> logEntry.toJson() + "\n").collect(Collectors.joining()));
+            Allure.addAttachment("Browser: Cookies", browser().driver().manage().getCookies().stream().map(cookie -> cookie.getName() + " : " + cookie.getValue() + "\n").collect(Collectors.joining()));
+            //Allure.addAttachment("Browser: HTML Page", "text/html", browser().driver().getPageSource(), ".html");
+        }
+    }
+
+    public void beforeFixtureStop(FixtureResult result) {
         Status status = result.getStatus();
         if (status == Status.FAILED || status == Status.BROKEN) {
             Allure.parameter("Browser: Failed URL", browser().getCurrentUrl());
