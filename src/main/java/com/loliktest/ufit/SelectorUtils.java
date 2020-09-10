f2c71ee893f0fef24e66df34cf428ccbc31a8b8e
dbudim <budim.dmitriy@pdffiller.team>
2020-09-10 16:28:41 +0300

    SelectorUtils make package private

--- src/main/java/com/loliktest/ufit/SelectorUtils.java
+++ src/main/java/com/loliktest/ufit/SelectorUtils.java
@@ -6,19 +6,19 @@ import org.openqa.selenium.By;
  * Created by dbudim on 10.09.2020
  */
 
-public class SelectorUtils {
+class SelectorUtils {
 
-    public static boolean isCss(String selector) {
+    static boolean isCss(String selector) {
         return !selector.contains("/");
     }
 
-    public static By getBy(String selector) {
+    static By getBy(String selector) {
         return isCss(selector)
                 ? By.cssSelector(selector)
                 : By.xpath(selector);
     }
 
-    public static boolean isSelectorCompatibleTo(String selector1, String selector2) {
+    static boolean isSelectorCompatibleTo(String selector1, String selector2) {
         return (isCss(selector1) && isCss(selector2)) | (!isCss(selector1) && !isCss(selector2));
     }
 }
